5322bbbe16537a71bf9cbc63c91aaf44ddb56d3a
Alex Suykov <alex.suykov@gmail.com>
2016-10-20 03:04:34 +0300

    rebuilding targets

--- make-support.mk
+++ make-support.mk
@@ -16,10 +16,43 @@ install:
 
 # clean only target stuff unless told otherwise
 clean:
-	rm -fr build-rv* files-rv* staging
+	rm -fr build-rv* files-rv* staging staged.list
 
 clean-all: clean
-	rm -fr build-* files-* staging
+	rm -fr build-* files-* staging staged.list
 
 unstage:
 	rm -fr staging
+
+# Re-run make in a selected directory, ignoring stamps.
+
+update-%:
+	test -f build-$*/stamp
+	rm build-$*/stamp
+	$(MAKE) build-$*/stamp $(if $(wildcard staging/$*),staging/$*)
+
+# Destructive partial targets
+
+rebuild-%: | build-%/stamp
+	rm -fr build-$*
+	$(MAKE) build-$*/stamp $(if $(wildcard staging/$*),staging/$*)
+
+refile-%:
+	test -f build-$*/stamp
+	rm -fr files-$*
+	$(MAKE) files-$*/stamp
+
+staged.list:
+	echo staging/rv* > $@
+
+restage: staged.list
+	rm -fr staging/{bin,exe,lib}
+	rm -fr staging/riscv*
+	rm -f staging/rv*
+	$(MAKE) $(shell cat $<)
+	rm -f $<
+
+restage-%:
+	test -f staging/$*
+	rm staging/$*
+	$(MAKE) staging/$*
