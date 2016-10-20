491f721298f9b8737232e550aac1a0659290e132
Alex Suykov <alex.suykov@gmail.com>
2016-10-20 03:03:25 +0300

    make clean and shorthand targets

--- make-support.mk
+++ make-support.mk
@@ -1,3 +1,7 @@
+# Fallback in case this has not been done before runnig make
+riscv-%/configure:
+	git submodule update --init $(dir $@)
+
 staging/%-tools: | staging/exe/ld staging/exe/gcc
 	scripts/makegcc $* "$($*)" "$($*-spec)"
 	scripts/makebin $* "$($*)"
@@ -5,3 +9,17 @@ staging/%-tools: | staging/exe/ld staging/exe/gcc
 
 install:
 	scripts/install "$(DESTDIR)" "$(prefix)"
+
+# Shorthand targets for individual parts: make rv64-gnu-libcxx
+%: build-%/stamp staging/%
+	@true
+
+# clean only target stuff unless told otherwise
+clean:
+	rm -fr build-rv* files-rv* staging
+
+clean-all: clean
+	rm -fr build-* files-* staging
+
+unstage:
+	rm -fr staging
