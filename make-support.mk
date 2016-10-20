d56bd541ab1ada885a24d97f2045db227ef9d4d4
Alex Suykov <alex.suykov@gmail.com>
2016-10-20 03:01:56 +0300

    make install

--- make-support.mk
+++ make-support.mk
@@ -2,3 +2,6 @@ staging/%-tools: | staging/exe/ld staging/exe/gcc
 	scripts/makegcc $* "$($*)" "$($*-spec)"
 	scripts/makebin $* "$($*)"
 	touch $@
+
+install:
+	scripts/install "$(DESTDIR)" "$(prefix)"
